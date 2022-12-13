a3f8cbf0554dabf4a8df575b8bfded7fd56f8580
Jesus Reynaldo <jezuz.of.murder@gmail.com>
2022-12-12 22:26:46 -0400

    First commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        byte variable1 =6;
+        short variable2 =100;
+        int variable3=2500;
+        long variable4=9651385;
+        char variable5='a';
+        String variable6="Esta es una cadena";
+        boolean variable7=true;
+        boolean variable8=false;
+        System.out.println("byte: "+variable1);
+        System.out.println("short: "+variable2);
+        System.out.println("int: "+variable3);
+        System.out.println("long: "+variable4);
+        System.out.println("char: "+variable5);
+        System.out.println("String: "+variable6);
+        System.out.println("boolean: "+variable7);
+        System.out.println("boolean: "+variable8);
+
+    }
+}
